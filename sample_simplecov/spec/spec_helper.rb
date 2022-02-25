826b5bb516881b35d45c73ae911e9d023dbbc6ce
snowfield702 <snowfield702@gmail.com>
2022-02-25 14:17:54 +0900

    Comment coverage to pull request

--- sample_simplecov/spec/spec_helper.rb
+++ sample_simplecov/spec/spec_helper.rb
@@ -15,7 +15,9 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  enable_coverage :branch
+end
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
